36088d769dc450d7bcb0f621577f011e8fbf224c
kkkk <2136267433@qq.com>
2024-11-19 16:47:15 +0800

    feat: 📓 note edit: new  update delete

--- components/SidebarNoteList.js
+++ components/SidebarNoteList.js
@@ -2,8 +2,8 @@ import SidebarNoteItem from "@/components/SidebarNoteItem";
 import { getAllNotes } from "@/lib/redis";
 
 export default async function NoteList() {
-  const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
-  await sleep(3000);
+  /* const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+  await sleep(3000); */
 
   const notes = await getAllNotes();
 
